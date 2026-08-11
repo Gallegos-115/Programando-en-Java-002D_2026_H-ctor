d825cd1557b675578502b63e6422c9c45d53bfa9
Gallegos-155 <hectorgallegos544@gmail..com>
2026-08-11 17:05:44 -0400

    Programando en java con perro, más chambeo

---
+++ src/Canil.java
@@ -0,0 +1,32 @@
+public class Canil {
+
+
+    public static void main(String[] args) {
+    Perro Perro_de_camilo = new Perro();
+    Perro Perro_de_Alexander = new Perro();
+    Perro Perro_de_rogelio= new Perro();
+
+    Perro_de_camilo.nombre="Porky";
+    Perro_de_camilo.edad=5;
+    Perro_de_camilo.raza="Quiltro";
+    Perro_de_camilo.peso=40;
+    Perro_de_camilo.vacuna=true;
+
+    Perro_de_Alexander.nombre="Sacha";
+    Perro_de_Alexander.edad=2;
+    Perro_de_Alexander.raza="Pastor Alemán";
+    Perro_de_Alexander.peso= 30.5;
+    Perro_de_Alexander.vacuna=true;
+
+    Perro_de_rogelio.nombre="Almendra";
+    Perro_de_rogelio.edad=4;
+    Perro_de_rogelio.raza="Quiltro";
+    Perro_de_rogelio.peso=4;
+    Perro_de_rogelio.vacuna=true;
+
+    System.out.println("Los siguiente animales han ingresado al canil");
+
+    }
+
+    
+}
