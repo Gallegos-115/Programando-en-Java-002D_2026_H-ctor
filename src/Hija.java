7b85dacf495b5b1a8285cb6f1dc2a031f92cb7fe
Gallegos-155 <hectorgallegos544@gmail..com>
2026-09-06 01:01:58 -0300

    Abstraccion

---
+++ src/Hija.java
@@ -0,0 +1,15 @@
+public class Hija {
+    //Para poder entender la abstracción debemos de tomar en cuenta que son los atributos de una entidad.
+    //¿Qué es una entidad?, una entidad puede ser cualquier cosa que tengas en mente, un perro, una persona y etc.
+    //Pero debes de tomar en cuenta sus atributos, un atributo común es la edad, nombre, altura y etc.
+    //Estos debes ser propios de tu clase a la cual quieras aplicar la abstracción.
+
+    //Un ejemplo de abstracción es:
+
+    String nombre;
+    int edad;
+    double altura; 
+
+    //Con esto tenemos listo la abstracción con los atributos correspondientes de nuestra entidad.
+    //Quede aclarar que puedes agregar todos los atributos que tu quieras.
+}
