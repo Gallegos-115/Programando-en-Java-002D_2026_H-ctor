306c6a5eae039024bf8d9ec47c5342f2397ae5b8
Gallegos-155 <hectorgallegos544@gmail..com>
2026-09-21 22:52:57 -0300

    Proyecto

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -3,10 +3,14 @@ public abstract class Vehiculo {
     private String Marca;
     private String Modelo;
     private int Año;
-    private double Costo;
+    private double Costo_Base;
     
-    Vehiculo(String patente){
+    Vehiculo(String patente, String marca, String modelo, int año, double costo_base){
         this.Patente = patente;
+        this.Marca = marca;
+        this.Modelo = modelo;
+        this.Año = año;
+        this.Costo_Base = costo_base;
     }
 
     public String getPatente(){
@@ -25,8 +29,55 @@ public abstract class Vehiculo {
         return this.Marca;
     }
 
-    public void setMarca(){
-        
+    public void setMarca(String marca){
+        if(Marca.isEmpty()){
+            System.out.println("Error: Patente vacía.");
+        }else{
+            this.Marca = marca;
+        }
+    }
+
+
+    public String getModelo(){
+        return this.Modelo;
+    }
+
+    public void setModelo(String modelo){
+        if(modelo.isEmpty()){
+            System.out.println("Error: Modelo vacío.");
+        }else{
+            this.Modelo = modelo;
+        }
+    }
+
+    public int getAño(){
+        return this.Año;
+    }
+
+    public void setAño(int años){
+        if(Año < 0){
+            System.out.println("Error: Dígitos negativos.");
+        }else{
+            this.Año = años;
+        }
+    } 
+
+    public double getCosto_Base(){
+        return this.Costo_Base;
+    }
+
+    public void setCosto_base(double costo_base){
+        if(Costo_Base < 0){
+            System.out.println("Error: Digítos negativos");
+        }else{
+            this.Costo_Base = costo_base;
+        }
+    }
+
+    public abstract int CalcularPrecio();
+
+    public String MostrarInformacion(){
+        return "La patente es: " + this.Patente + "| " + " Modelo : " + this.Modelo + "| " + " Marca" + this.Marca + "| " + "Año " + this.Año + "| Precio Base: " + this.Costo_Base;        
     }
     
 }
